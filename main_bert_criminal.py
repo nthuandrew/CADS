328841986558668231afe68953331aec87dd333f
Carolin <49471458+bliss32@users.noreply.github.com>
2021-10-14 12:06:22 +0800

    Updated main_bert_criminal.py with factor analysis.

--- main_bert_criminal.py
+++ main_bert_criminal.py
@@ -2,37 +2,52 @@ from module.util import *
 from class_Segmentation import Segmentation
 from class_BertForClassification_Wrapper import Bert_Wrapper
 
-criminal_type = 'gun'
+
+# model_setting = {'criminal_type': 'gun', 'classify': 'sentence'}
+model_setting = {'criminal_type': 'gun', 'classify': 'factor', 'factor_lst': ['犯罪後之態度', '犯罪所生之危險或違反義務之程度', '被告之品行', '其他審酌事項']}
+
 
 # Segmentation
-df = pd.read_excel(f'data/raw/data_criminal_{criminal_type}.xlsx')
-seg = Segmentation(type="bert")
-# modify col names
-if criminal_type == 'sex':
-    categorical_info = ['法條', '犯罪後之態度', '犯罪之手段與所生損害', '被害人的態度', '被告之品行', '其他審酌事項', '有利', '中性', '不利']
-elif criminal_type=='gun':
-    categorical_info = ['法條', '犯罪後之態度', '犯罪所生之危險或違反義務之程度', '被告之品行', '其他審酌事項', '有利', '中性', '不利']
-elif criminal_type=='drug':
-    categorical_info = ['法條', '犯罪後之態度', '犯罪所生之危險或損害或違反義務之程度', '被告之品行', '其他審酌事項', '有利', '中性', '不利']
-# run
-df = seg.segment_criminal_sentiment_analysis_articles_wrapper(df, categorical_info, criminal_type)
-del df, seg
-gc.collect()
+# df = pd.read_excel(f'data/raw/data_criminal_%s.xlsx' % model_setting['criminal_type'])
+# seg = Segmentation(type="bert")
+# # modify col names
+# if criminal_type == 'sex':
+#     categorical_info = ['法條', '犯罪後之態度', '犯罪之手段與所生損害', '被害人的態度', '被告之品行', '其他審酌事項', '有利', '中性', '不利']
+# elif criminal_type=='gun':
+#     categorical_info = ['法條', '犯罪後之態度', '犯罪所生之危險或違反義務之程度', '被告之品行', '其他審酌事項', '有利', '中性', '不利']
+# elif criminal_type=='drug':
+#     categorical_info = ['法條', '犯罪後之態度', '犯罪所生之危險或損害或違反義務之程度', '被告之品行', '其他審酌事項', '有利', '中性', '不利']
+# # run
+# df = seg.segment_criminal_sentiment_analysis_articles_wrapper(df, categorical_info, model_setting['criminal_type'])
+# del df, seg
+# gc.collect()
 
 
-# BERT: Classification for criminal sentiment analysis
-seed_list = [1234, 5678, 7693145, 947, 13, 27, 1, 5, 9, 277]
-df = pd.read_pickle(f'./data/cleaned/criminal_{criminal_type}_seg_bert.pkl')
-# for i in range(10):
-#     print("Start test:", )
-#     bw = Bert_Wrapper(num_labels = 3, seed = seed_list[])
-#     trainloader, validloader, testloader = bw.prepare_criminal_sentiment_analysis_dataloader(df)
-#     bw.initialize_training()
-#     bw.train()
-#     bw.evaluate(path=f"{criminal_type}.txt")
-bw = Bert_Wrapper(num_labels = 3, seed = seed_list[0])
-trainloader, validloader, testloader = bw.prepare_criminal_sentiment_analysis_dataloader(df)
-bw.initialize_training()
-bw.train()
-bw.evaluate(path=f"{criminal_type}.txt")
+if model_setting['classify'] == 'sentence':
+    # BERT: Classification for criminal sentiment analysis
+    seed_list = [1234, 5678, 7693145, 947, 13, 27, 1, 5, 9, 277]
+    df = pd.read_pickle(f'./data/cleaned/criminal_%s_seg_bert.pkl' % model_setting['criminal_type'])
+    # for i in range(10):
+    #     print("Start test:", )
+    #     bw = Bert_Wrapper(num_labels = 3, seed = seed_list[])
+    #     trainloader, validloader, testloader = bw.prepare_criminal_sentiment_analysis_dataloader(df)
+    #     bw.initialize_training()
+    #     bw.train()
+    #     bw.evaluate(path=f"{criminal_type}.txt")
+    bw = Bert_Wrapper(num_labels = 3, seed = seed_list[0])
+    trainloader, validloader, testloader = bw.prepare_criminal_sentiment_analysis_dataloader(df)
+    bw.initialize_training()
+    bw.train()
+    bw.evaluate(path=f"%s.txt" % model_setting['criminal_type'])
 
+elif model_setting['classify'] == 'factor':
+    # BERT: Classification for criminal factor classification #############
+    df = pd.read_pickle(f'./data/cleaned/criminal_%s_seg_bert.pkl' % model_setting['criminal_type'])
+    for fac in model_setting['factor_lst']:
+        bw = Bert_Wrapper(num_labels = 2)
+        trainloader, validloader, testloader = bw.prepare_criminal_judgement_factor_dataloader(df, fac)
+        bw.initialize_training()
+        bw.train()
+        bw.evaluate(path=f"%s_%s.txt" % (model_setting['criminal_type'], fac))
+        del trainloader, validloader, testloader , bw
+        gc.collect()
